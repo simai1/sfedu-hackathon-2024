9e2197f9ef5c1d52ab118b1e9f4e30f6c40e9dc0
Sima <mksim3uchnev@gmail.com>
2024-11-23 13:19:53 +0300

    feat(api): add floors dto for get buildings

--- api/src/dtos/building.dto.ts
+++ api/src/dtos/building.dto.ts
@@ -1,15 +1,16 @@
 import Building from '../models/building';
+import FloorDto from './floor.dto';
 
 export default class BuildingDto {
     id!: string;
     name!: string;
     address!: string;
-    floors?: string[];
+    floors?: FloorDto[];
 
     constructor(model: Building) {
         this.id = model.id;
         this.name = model.name;
         this.address = `г. ${model.addressCity}, ${model.addressOther}`;
-        this.floors = model.Floors?.map(m => m.name);
+        this.floors = model.Floors ? model.Floors.map(f => new FloorDto(f)) : undefined;
     }
 }
