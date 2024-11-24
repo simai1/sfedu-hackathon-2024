1370cd8e24b14812e1f52e21bca229d8152ee679
Никита <100412266+Niks6455@users.noreply.github.com>
2024-11-24 11:07:42 +0300

    feat(client): Закрепление допиливание адаптива

--- web/src/components/HomePageProfileClicker/HomePageProfileClicker.jsx
+++ web/src/components/HomePageProfileClicker/HomePageProfileClicker.jsx
@@ -3,48 +3,43 @@ import styles from "./HomePageProfileClicker.module.scss";
 import { GetProfile, SwitchRole } from '../../API/ApiRequest';
 import DataContext from '../../context';
 
-
 function HomePageProfileClicker() {
     const context = useContext(DataContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef(null); // Create a ref for the dropdown
+
     const handleClick = () => {
         SwitchRole().then((res) => {
-            if(res?.status === 200){
+            if (res?.status === 200) {
                 funCheckRole();
             }
-        })
+        });
     };
 
     const funCheckRole = () => {
         GetProfile().then((resp) => {
-            if(resp?.status === 200){
-                if(resp.data.data.role === 2){
-                    context.setRole("Администратор");
-                }else{
-                    context.setRole("Пользователь");
-                }
+            if (resp?.status === 200) {
+                context.setRole(resp.data.data.role === 2 ? "Администратор" : "Пользователь");
             }
-        })
-    }
+        });
+    };
 
     useEffect(() => {
         funCheckRole();
-    },[])
+    }, []);
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-      setIsOpen(!isOpen);
-    };
-
-    const dropdownRef = useRef(null); // Create a ref for the dropdown
+    useEffect(() => {
+        console.log("isOpen", isOpen);
+    }, [isOpen]);
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            // Check if the click is outside the dropdown and the burger icon
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target) && !event.target.closest(`.${styles.burgerIcon}`)) {
                 setIsOpen(false);
             }
         };
-    
+
         document.addEventListener("mousedown", handleClickOutside);
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
@@ -52,10 +47,10 @@ function HomePageProfileClicker() {
     }, []);
 
     const clickLi = (value) => {
-        context.setActiveTable(value)
-        context.getTableData(value)
+        context.setActiveTable(value);
+        context.getTableData(value);
         setIsOpen(false);
-    }
+    };
 
     return ( 
         <div className={styles.HomePageProfileClicker}>
@@ -63,8 +58,8 @@ function HomePageProfileClicker() {
             <div className={styles.HomePageProfileClickerContainer}>
                 <p>{context.role}</p>
                 <div
-                    className={`${styles.switch} ${context.role==="Администратор" ? styles['switch--checked'] : ''}`}
-                    onClick={() => handleClick()}
+                    className={`${styles.switch} ${context.role === "Администратор" ? styles['switch--checked'] : ''}`}
+                    onClick={handleClick}
                 >
                     <div className={styles.switch__circle} />
                 </div>
@@ -72,7 +67,7 @@ function HomePageProfileClicker() {
             
             <div className={styles.BoorgerMobile}>
                 <div className={styles.burgerMenu}>
-                    <div className={`${styles.burgerIcon} ${isOpen ? styles.open : ''}`} onClick={toggleMenu}>
+                    <div className={`${styles.burgerIcon} ${isOpen ? styles.open : ''}`} onClick={() => setIsOpen(!isOpen)}>
                         <span className={styles.line}></span>
                         <span className={styles.line}></span>
                         <span className={styles.line}></span>
@@ -80,18 +75,16 @@ function HomePageProfileClicker() {
                     {isOpen && (
                         <div className={styles.menu} ref={dropdownRef}>
                             <ul>
-                                <li onClick={() => clickLi("office")}><span style={{borderBottom: context?.activeTable === "office" &&  "1px solid #1A7D78", color: context?.activeTable === "office" && "#1A7D78" }}>Мои офисы</span></li>
-                                <li onClick={() => clickLi("Staff")} ><span style={{borderBottom: context?.activeTable === "Staff" &&  "1px solid #1A7D78", color: context?.activeTable === "Staff" && "#1A7D78" }}>Сотрудники</span></li>
-                                <li onClick={() => clickLi("Equipment")} ><span style={{borderBottom: context?.activeTable === "Equipment" && "1px solid #1A7D78", color: context?.activeTable === "Equipment" && "#1A7D78" }}>Список оборудования</span></li>
+                                <li onClick={() => clickLi("office")}><span style={{ borderBottom: context?.activeTable === "office" ? "1px solid #1A7D78" : "none", color: context?.activeTable === "office" ? "#1A7D78" : "inherit" }}>Мои офисы</span></li>
+                                <li onClick={() => clickLi("Staff")}><span style={{ borderBottom: context?.activeTable === "Staff" ? "1px solid #1A7D78" : "none", color: context?.activeTable === "Staff" ? "#1A7D78" : "inherit" }}>Сотрудники</span></li>
+                                <li onClick={() => clickLi("Equipment")}><span style={{ borderBottom: context?.activeTable === "Equipment" ? "1px solid #1A7D78" : "none", color: context?.activeTable === "Equipment" ? "#1A7D78" : "inherit" }}>Список оборудования</span></li>
                             </ul>
                         </div>
                     )}
                 </div>
             </div>
-           
         </div>
     );
 }
-
 
 export default HomePageProfileClicker;
